ee06ffeffa4bdc13fa2e9d69b05b751cf050a09d
ayelabilal <ayelabilal944@gmail.com>
2024-03-01 16:38:52 +0500

    continue

---
+++ app.js
@@ -0,0 +1,23 @@
+// USER INPUT & CONDITIONAL STATEMENT
+// ques 1
+var name = +prompt ("City")
+if (city){alert("Welcome to city of lights");}
+
+// ques 2
+var gender = +prompt ("gender");
+var male = "Good Morning Sir"
+var female = "Good Morning Maam"
+if (male){alert("Good Morning Sir")}
+else if (female){alert("Good")};
+
+
+// ques 3
+
+
+// ques 4
+
+// ques 5
+
+// ques 6
+
+// ques 7
