b0dbac4783f3162a8227b374808351c4cd36ac5c
ayelabilal <ayelabilal944@gmail.com>
2024-03-02 17:04:08 +0500

    continue

--- app.js
+++ app.js
@@ -1,23 +1,93 @@
 // USER INPUT & CONDITIONAL STATEMENT
 // ques 1
-var name = +prompt ("City")
-if (city){alert("Welcome to city of lights");}
+var city = prompt ("City")
+if (city === "Karachi"){alert("Welcome to city of lights!")}
+
 
 // ques 2
-var gender = +prompt ("gender");
-var male = "Good Morning Sir"
-var female = "Good Morning Maam"
-if (male){alert("Good Morning Sir")}
-else if (female){alert("Good")};
-
+var gender = prompt ("gender");
+if (gender === "male"){alert("Good Morning Sir")}
+else if (gender === "female"){alert("Good Morning Maam")}
+else {alert("Good Morning")}
 
 // ques 3
-
+var trafficSignal = prompt ("color")
+if(trafficSignal === "Red"){alert("Must Stop")}
+else if (trafficSignal === "Yellow"){alert("Ready to Move")}
+else if (trafficSignal === "green"){alert("Move Now")}
+else{alert("Error");}
 
 // ques 4
+var fuel = prompt("remaining fuel")
+if (fuel >=0.25) {alert("Please refill the fuel in your car")} ;
+// // ques 5
+// var a = 4;
+// if(++a === 5){alert("given condition for variable a is true");}
+ 
+// var b = 82;
+// if (b++ === 83){alert("given condition for variable b is true");}
 
-// ques 5
+// var c = 12
+// if (c++ === 13){alert("condition 1 is true");}
 
-// ques 6
+// if (c === 13){  alert("condition 2 is true");}
+
+// if (++c < 14){ alert("condition 3 is true"); }
+
+// if(c === 14){ alert("condition 4 is true");}
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){alert("The cost equals");}
+
+// if (true){alert("True");}
+
+// if (false){alert("False");}
+
+// if("car" < "cat"){alert("car is smaller than cat");}
+
+// 6
 
 // ques 7
+var number = prompt("guess the secret number")
+if (number === "5"){alert("Bingo! Correct number")}
+else if (number <=5){alert("Close enough to the correct answer")}
+
+// ques 8
+var number = prompt("enter number")
+if (number %3 === 0){alert("correct")}
+else {alert("incorrect");}
+
+// 9
+var number = prompt("even or odd")
+if (number *3){alert("even")}
+else if (number *2){alert("odd")}
+
+// 10
+var temperature = prompt("Enter Temperature.")
+if (temperature >40){alert("It is too hot outside.")}
+if (temperature >30){alert("The Weather today is Normal.")}
+if (temperature >20){alert("Today's Weather is cool.")}
+if (temperature >10){alert("OMG! Today's weather is so cool.")}
+
+// 11
+
+// IF...ELSE & ELSE IF STATEMENT, TESTING SET OF CONDITONS
+
+// 1
+
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+
+                    //  CHAPTER 10(IF STATEMENTs)
+
+// 1
+var city = "Karachi"
+if(city === "Karachi"){console.log("The city of lights")} 
+
+// 2
