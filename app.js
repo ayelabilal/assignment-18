207b1d83b3fe5b11e1998fa2a6656101ce63a35e
ayelabilal <ayelabilal944@gmail.com>
2024-03-04 01:45:25 +0500

    complete

--- app.js
+++ app.js
@@ -55,14 +55,14 @@ if (number === "5"){alert("Bingo! Correct number")}
 else if (number <=5){alert("Close enough to the correct answer")}
 
 // ques 8
-var number = prompt("enter number")
+var number = prompt("enter number divisible by 3")
 if (number %3 === 0){alert("correct")}
 else {alert("incorrect");}
 
 // 9
 var number = prompt("even or odd")
-if (number *3){alert("even")}
-else if (number *2){alert("odd")}
+if (number %3 === 0){alert("odd")}
+else if (number %2 === 0){alert("even")}
 
 // 10
 var temperature = prompt("Enter Temperature.")
@@ -79,9 +79,21 @@ if (temperature >10){alert("OMG! Today's weather is so cool.")}
 
 // 2
 // 3
+var number = prompt("enter number")
+if (number %2 === 0){alert("positive")}
+else if (number %3 === 0){alert("negative")}
+else {alert("zero")};
+
 // 4
+var char = "f"
+if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u"){document.write("true")}
+else {document.write(false)}
+
 // 5
 // 6
+var hours = prompt("13")
+if(hours <18){alert("Good day")}
+else{alert("Good evening")}
 // 7
 
                     //  CHAPTER 10(IF STATEMENTs)
@@ -90,4 +102,26 @@ if (temperature >10){alert("OMG! Today's weather is so cool.")}
 var city = "Karachi"
 if(city === "Karachi"){console.log("The city of lights")} 
 
-// 2
+// 3
+var zipCode = prompt("zip code")
+if (zipCode === "10010"){alert("Karachi")}
+else {alert("Please write correct city")};
+
+// chapter 11 (Comparison Operators)
+
+// 1
+var number = prompt("number")
+if (number != "22"){alert("unequal")}
+
+// chapter 12 (if..else ans else if statement)
+
+// 3
+var a = prompt("number")
+if (a === 10){alert("a is 10")}
+else {alert("The correct value of a is 10")}
+
+// 4
+var city = prompt("enter your city")
+if(city === "karachi"){alert("it is karachi")}
+else {alert("lahore")}
+
